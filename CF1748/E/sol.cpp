09ddde26d4277feb3699899314425a464d58b188
shivom <shivom8787butterbus@gmail.com>
2024-10-08 16:17:01 +0530

    i am a genius

---
+++ CF1748/E/sol.cpp
@@ -0,0 +1,425 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define pb push_back
+template <typename T>
+using Vec = vector<T>;
+ 
+#ifndef ONLINE_JUDGE
+#include "/home/shivom/Downloads/CP/DataStructures/debug.cpp"
+#else
+#define dbg(...) 42
+#endif
+
+// ============
+
+#include <cassert>
+#include <iostream>
+#include <type_traits>
+
+// ============
+
+constexpr bool is_prime(unsigned n) {
+    if (n == 0 || n == 1) {
+        return false;
+    }
+    for (unsigned i = 2; i * i <= n; ++i) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+constexpr unsigned mod_pow(unsigned x, unsigned y, unsigned mod) {
+    unsigned ret = 1, self = x;
+    while (y != 0) {
+        if (y & 1) {
+            ret = (unsigned) ((unsigned long long) ret * self % mod);
+        }
+        self = (unsigned) ((unsigned long long) self * self % mod);
+        y /= 2;
+    }
+    return ret;
+}
+
+template <unsigned mod>
+constexpr unsigned primitive_root() {
+    static_assert(is_prime(mod), "`mod` must be a prime number.");
+    if (mod == 2) {
+        return 1;
+    }
+
+    unsigned primes[32] = {};
+    int it = 0;
+    {
+        unsigned m = mod - 1;
+        for (unsigned i = 2; i * i <= m; ++i) {
+            if (m % i == 0) {
+                primes[it++] = i;
+                while (m % i == 0) {
+                    m /= i;
+                }
+            }
+        }
+        if (m != 1) {
+            primes[it++] = m;
+        }
+    }
+    for (unsigned i = 2; i < mod; ++i) {
+        bool ok = true;
+        for (int j = 0; j < it; ++j) {
+            if (mod_pow(i, (mod - 1) / primes[j], mod) == 1) {
+                ok = false;
+                break;
+            }
+        }
+        if (ok)
+            return i;
+    }
+    return 0;
+}
+
+// y >= 1
+template <typename T>
+constexpr T safe_mod(T x, T y) {
+    x %= y;
+    if (x < 0) {
+        x += y;
+    }
+    return x;
+}
+
+// y != 0
+template <typename T>
+constexpr T floor_div(T x, T y) {
+    if (y < 0) {
+        x *= -1;
+        y *= -1;
+    }
+    if (x >= 0) {
+        return x / y;
+    } else {
+        return -((-x + y - 1) / y);
+    }
+}
+
+// y != 0
+template <typename T>
+constexpr T ceil_div(T x, T y) {
+    if (y < 0) {
+        x *= -1;
+        y *= -1;
+    }
+    if (x >= 0) {
+        return (x + y - 1) / y;
+    } else {
+        return -(-x / y);
+    }
+}
+// ============
+
+template <unsigned mod>
+class ModInt {
+    static_assert(mod != 0, "`mod` must not be equal to 0.");
+    static_assert(
+        mod < (1u << 31),
+        "`mod` must be less than (1u << 31) = 2147483648.");
+
+    unsigned val;
+
+  public:
+    static constexpr unsigned get_mod() {
+        return mod;
+    }
+
+    constexpr ModInt() : val(0) {}
+    template <typename T, std::enable_if_t<std::is_signed_v<T>> * = nullptr>
+    constexpr ModInt(T x) : val((unsigned) ((long long) x % (long long) mod + (x < 0 ? mod : 0))) {}
+    template <typename T, std::enable_if_t<std::is_unsigned_v<T>> * = nullptr>
+    constexpr ModInt(T x) : val((unsigned) (x % mod)) {}
+
+    static constexpr ModInt raw(unsigned x) {
+        ModInt<mod> ret;
+        ret.val = x;
+        return ret;
+    }
+
+    constexpr unsigned get_val() const {
+        return val;
+    }
+
+    constexpr ModInt operator+() const {
+        return *this;
+    }
+    constexpr ModInt operator-() const {
+        return ModInt<mod>(0u) - *this;
+    }
+
+    constexpr ModInt &operator+=(const ModInt &rhs) {
+        val += rhs.val;
+        if (val >= mod)
+            val -= mod;
+        return *this;
+    }
+    constexpr ModInt &operator-=(const ModInt &rhs) {
+        if (val < rhs.val)
+            val += mod;
+        val -= rhs.val;
+        return *this;
+    }
+    constexpr ModInt &operator*=(const ModInt &rhs) {
+        val = (unsigned long long)val * rhs.val % mod;
+        return *this;
+    }
+    constexpr ModInt &operator/=(const ModInt &rhs) {
+        val = (unsigned long long)val * rhs.inv().val % mod;
+        return *this;
+    }
+
+    friend constexpr ModInt operator+(const ModInt &lhs, const ModInt &rhs) {
+        return ModInt<mod>(lhs) += rhs;
+    }
+    friend constexpr ModInt operator-(const ModInt &lhs, const ModInt &rhs) {
+        return ModInt<mod>(lhs) -= rhs;
+    }
+    friend constexpr ModInt operator*(const ModInt &lhs, const ModInt &rhs) {
+        return ModInt<mod>(lhs) *= rhs;
+    }
+    friend constexpr ModInt operator/(const ModInt &lhs, const ModInt &rhs) {
+        return ModInt<mod>(lhs) /= rhs;
+    }
+
+    constexpr ModInt pow(unsigned long long x) const {
+        ModInt<mod> ret = ModInt<mod>::raw(1);
+        ModInt<mod> self = *this;
+        while (x != 0) {
+            if (x & 1)
+                ret *= self;
+            self *= self;
+            x >>= 1;
+        }
+        return ret;
+    }
+    constexpr ModInt inv() const {
+        static_assert(is_prime(mod), "`mod` must be a prime number.");
+        assert(val != 0);
+        return this->pow(mod - 2);
+    }
+
+    friend std::istream &operator>>(std::istream &is, ModInt<mod> &x) {
+        long long val;
+        is >> val;
+        x.val = val % mod + (val < 0 ? mod : 0);
+        return is;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const ModInt<mod> &x) {
+        os << x.val;
+        return os;
+    }
+
+    friend bool operator==(const ModInt &lhs, const ModInt &rhs) {
+        return lhs.val == rhs.val;
+    }
+
+    friend bool operator!=(const ModInt &lhs, const ModInt &rhs) {
+        return lhs.val != rhs.val;
+    }
+};
+
+[[maybe_unused]] constexpr unsigned mod998244353 = 998244353;
+[[maybe_unused]] constexpr unsigned mod1000000007 = 1000000007;
+
+// ============
+// ============
+
+#include <vector>
+#include <cassert>
+
+template <typename T>
+class FactorialTable {
+    std::vector<T> fac;
+    std::vector<T> ifac;
+
+  public:
+    FactorialTable() : fac(1, T(1)), ifac(1, T(1)) {}
+
+    FactorialTable(int n) : fac(n + 1), ifac(n + 1) {
+        assert(n >= 0);
+        fac[0] = T(1);
+        for (int i = 1; i <= n; ++i) {
+            fac[i] = fac[i - 1] * T(i);
+        }
+        ifac[n] = T(1) / T(fac[n]);
+        for (int i = n; i > 0; --i) {
+            ifac[i - 1] = ifac[i] * T(i);
+        }
+    }
+
+    void resize(int n) {
+        int old = n_max();
+        if (n <= old) {
+            return;
+        }
+        fac.resize(n + 1);
+        for (int i = old + 1; i <= n; ++i) {
+            fac[i] = fac[i - 1] * T(i);
+        }
+        ifac.resize(n + 1);
+        ifac[n] = T(1) / T(fac[n]);
+        for (int i = n; i > old; --i) {
+            ifac[i - 1] = ifac[i] * T(i);
+        }
+    }
+
+    inline int n_max() const {
+        return (int) fac.size() - 1;
+    }
+
+    inline T fact(int n) const {
+        assert(n >= 0 && n <= n_max());
+        return fac[n];
+    }
+
+    inline T inv_fact(int n) const {
+        assert(n >= 0 && n <= n_max());
+        return ifac[n];
+    }
+
+    inline T choose(int n, int k) const {
+        assert(k <= n_max() && n <= n_max());
+        if (k > n || k < 0) {
+            return T(0);
+        }
+        return fac[n] * ifac[k] * ifac[n - k];
+    }
+
+    inline T multi_choose(int n, int k) const {
+        assert(n >= 1 && k >= 0 && k + n - 1 <= n_max());
+        return choose(k + n - 1, k);
+    }
+
+    inline T n_terms_sum_k(int n, int k) const {
+        assert(n >= 0);
+        if (k < 0) {
+            return T(0);
+        }
+        if (n == 0) {
+            return k == 0 ? T(1) : T(0);
+        }
+        return choose(n + k - 1, n - 1);
+    }
+};
+using Mint = ModInt<mod1000000007>;
+void __print(Mint x) {
+    cerr << x;
+}
+// ============
+Mint one = 1;
+auto solve(ll n, ll m, vector<ll> a) {
+
+    vector<ll> L(n, 0), R(n, 0), nge(n, n), pge(n, -1);
+
+    deque<ll> dq;
+    for (ll i = 0; i < n; i++) {
+        while (!dq.empty() && a[i] > a[dq.back()]) {
+            dq.pop_back();
+        }
+        pge[i] = (dq.empty() ? -1ll : dq.back());
+        dq.push_back(i);
+    }
+    dq.clear();
+    for (ll i = n - 1; i >= 0; i--) {
+        while (!dq.empty() && a[i] >= a[dq.back()]) {
+            dq.pop_back();
+        }
+        nge[i] = (dq.empty() ? n : dq.back());
+        dq.push_back(i);
+    }
+
+    for (ll i = 0; i < n; i++) {
+        L[i] = pge[i] + 1;
+    }
+    for (ll i = 0; i < n; i++) {
+        R[i] = nge[i] - 1;
+    }
+
+    dbg(pge);
+    dbg(nge);
+
+    dbg(L);
+    dbg(R);
+
+    vector<ll> endIdx[n];
+
+    for (ll i = 0; i < n; i++) {
+        endIdx[R[i]].pb(i);
+    }
+    vector<ll> si(n, -1), pi(n, -1);
+    for (ll i = 0; i < n; i++) {
+        if (i == 0 || endIdx[i-1].empty()) {
+            si[i] = -1;
+        } else {
+            si[i] = endIdx[i-1][0];
+        }
+    }
+
+
+    dbg(si);
+
+    for (ll i = 0; i < n; i++) {
+        pi[i] = -1;
+        auto it = upper_bound(begin(endIdx[R[i]]),end(endIdx[R[i]]),i);
+        if (it == end(endIdx[R[i]])) {
+
+        } else {
+            pi[i] = *it;
+        }
+    }
+
+    dbg(pi);
+
+    vector<ll> I(n, 0);
+    for (ll i = 0; i < n; i++) I[i] = i;
+
+    sort(begin(I),end(I), [&] (auto x, auto y) {
+        return R[x] - L[x] < R[y] - L[y];
+    });
+
+    Vec<Vec<Mint>> just(n, Vec<Mint> (m + 1, 0)), upto(n, Vec<Mint> (m + 1, 0));
+
+    dbg(I);
+
+    for (auto& i : I) {
+        for (ll k = 1; k <= m; k++) {
+            if (si[i] == -1) {
+                just[i][k] = one;
+            } else {
+                just[i][k] = upto[si[i]][k-1];
+            }
+            upto[i][k] = just[i][k] * (pi[i] == -1 ?
+                one : upto[pi[i]][k]
+            );
+            upto[i][k] += upto[i][k-1];
+        }
+    }
+
+    return upto[endIdx[n-1].front()][m].get_val();
+}
+
+int main() {
+    ll T;
+    cin >> T;
+    for (ll tc = 1; tc <= T; tc++) {
+        ll n, m;
+        cin >> n >> m;
+        vector<ll> a(n);
+        for (ll i = 0; i < n; i++)
+            cin >> a[i];
+        
+        auto res = solve(n, m, a);
+        cout << res << "\n";
+    }
+    return 0;
+}
--- MetaHackerCup/2023/Round1/C2/sol.cpp
+++ MetaHackerCup/2023/Round1/C2/sol.cpp
@@ -1,123 +1,17 @@
-// #define ONLINE_JUDGE
+#define ONLINE_JUDGE
 #include <bits/stdc++.h>
 using namespace std;
-using ll = int;
+using ll = long long;
 #define ALL(x) begin(x), end(x)
 #define pb push_back
 // =================
 
-// Debugging template
-void __print(int x) { cerr << x; }
-void __print(long x) { cerr << x; }
-void __print(long long x) { cerr << x; }
-void __print(unsigned x) { cerr << x; }
-void __print(unsigned long x) { cerr << x; }
-void __print(unsigned long long x) { cerr << x; }
-void __print(float x) { cerr << x; }
-void __print(double x) { cerr << x; }
-void __print(long double x) { cerr << x; }
-void __print(char x) { cerr << '\'' << x << '\''; }
-void __print(const char *x) { cerr << '\"' << x << '\"'; }
-void __print(const string &x) { cerr << '\"' << x << '\"'; }
-void __print(bool x) { cerr << (x ? "true" : "false"); }
-template <typename A>
-void __print(const A &x);
-template <typename A, typename B>
-void __print(const pair<A, B> &p);
-template <typename... A>
-void __print(const tuple<A...> &t);
-template <typename T>
-void __print(stack<T> s);
-template <typename T>
-void __print(queue<T> q);
-template <typename T, typename... U>
-void __print(priority_queue<T, U...> q);
-/*
-void __print(Mint x) {
-	cerr << x;
-}
-*/
-template <typename A>
-void __print(const A &x) {
-	bool first = true;
-	cerr << '{';
-	for (const auto &i : x) {
-		cerr << (first ? "" : ","), __print(i);
-		first = false;
-	}
-	cerr << '}';
-}
-template <typename A, typename B>
-void __print(const pair<A, B> &p) {
-	cerr << '(';
-	__print(p.first);
-	cerr << ',';
-	__print(p.second);
-	cerr << ')';
-}
-template <typename... A>
-void __print(const tuple<A...> &t) {
-	bool first = true;
-	cerr << '(';
-	apply([&first](const auto & ...args) { ((cerr << (first ? "" : ","), __print(args), first = false), ...); }, t);
-	cerr << ')';
-}
-template <typename T>
-void __print(stack<T> s) {
-	vector<T> debugVector;
-	while (!s.empty()) {
-		T t = s.top();
-		debugVector.push_back(t);
-		s.pop();
-	}
-	reverse(debugVector.begin(), debugVector.end());
-	__print(debugVector);
-}
-template <typename T>
-void __print(queue<T> q) {
-	vector<T> debugVector;
-	while (!q.empty()) {
-		T t = q.front();
-		debugVector.push_back(t);
-		q.pop();
-	}
-	__print(debugVector);
-}
-template <typename T, typename... U>
-void __print(priority_queue<T, U...> q) {
-	vector<T> debugVector;
-	while (!q.empty()) {
-		T t = q.top();
-		debugVector.push_back(t);
-		q.pop();
-	}
-	__print(debugVector);
-}
-void _print() { cerr << "]\n"; }
-template <typename Head, typename... Tail>
-void _print(const Head &H, const Tail &...T) {
-	__print(H);
-	if (sizeof...(T))
-		cerr << ", ";
-	_print(T...);
-}
-#ifndef ONLINE_JUDGE
-#define dbg(...) cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
-
-// =================
 template <typename T>
 void makeUniq(vector<T>& vec) {
     sort(begin(vec),end(vec));
     vec.resize(unique(ALL(vec)) - vec.begin());
 }
-auto print(auto a) {
-    for (auto& x : a) {
-        cout << x << " ";
-    }   cout << "\n";
-}
+
 auto bf(auto n, auto a, auto q, auto x) {
     vector<ll> vals;
     auto calc = [&] (vector<ll> a) {
@@ -135,137 +29,47 @@ auto bf(auto n, auto a, auto q, auto x) {
     };
     
     vals.push_back(calc(a));
-    print(a);
+    
     for (auto&v : x) {
         for (ll i = v - 1; i < n; i += v) {
             a[i] ^= 1;
         }
-        print(a);
         vals.push_back(calc(a));
     }
     ll fans = 0;
     
-    // dbg(vals);
-    return vals;
     for (ll i = 1; i < vals.size(); i++) {
         fans+=vals[i];
     }
-    // return fans;
+    return fans;
 }
-/*
-    How many times we have used it previously is what we want to know
-*/
 
 auto solve(auto n, auto a, auto q, auto x) {
-    map<ll, ll> mp;
-    vector<ll> vals;
-    auto calc = [&] (vector<ll> a) {
-        ll fans = 0;
-        for (ll i = 0; i < n; i++) {
-            if (a[i] == 1) {
-                fans++;
-                ll v = i + 1;
-                for (ll i = v - 1; i < n; i += v) {
-                    a[i] ^= 1;
-                }
-            }
-        }
-        return fans;
-    };
-    
-    vals.push_back(calc(a));
-
-    vector<ll> cnt(n+1, 0);
-
-    for (auto&v : x) {
-        cnt[v]++;
-        ll sum = 0;
-        dbg(v);
-        vector<ll> F;
-        for (ll i = 1; i <= sqrt(v); i++) {
-
-            dbg(i);
-            if (v % i == 0) {
-                ll fac = i;
-                F.pb(i);
-                // if (cnt[fac]&1) sum++;
-                if (v / i != i) {
-                    ll fac = v / i;
-                    F.pb(v/i);
-                    // if (cnt[fac]&1) sum++;
-                }
-            }
-        }
-        sort(begin(F),end(F));
-        sum = 0;
-        ll zero = 0, sex=0, fv = -1;
-        dbg(F); 
-        for (auto& f : F) {
-            sum += cnt[f];
-            ll actAs = -1;
-            if (sum & 1) {
-                if (a[f-1] == 0) {
-                    actAs = 1;
-                }  else {
-                    actAs = 0;
-                }
-            } else {
-                if (a[f-1] == 0) {
-                    actAs = 0;
-                } else {
-                    actAs = 1;
-                }
-            }
-            // if (actAs != zero) {
-            //     zero = actAs;
-            // }
-            // if (actAs != zero) {
-            //     sex++;
-            //     zero=actAs; 
-            // }
-            sex ^= actAs;
-            if (fv == -1) {
-                fv = actAs;
-            }
-            dbg(f, actAs);
-        }
-        dbg(sum);
-        dbg(zero);
-        dbg(cnt);
-        dbg(a);
-        dbg(sex);
-        fv = -1;
-        if (mp[1]&1) {
-            fv = (a[0]^1);
-        }else{
-            fv = a[0];
-        }
-        dbg(fv, zero);
-        sex = fv == zero;
-        if (sex&1) {
-            if (zero == 0) {
-                vals.push_back(vals.back() + 1);
-            } else {
-                vals.push_back(vals.back() - 1);
-            }
+    ll cur = 0;
+    vector<ll> op(n, 0);
+    for (ll i = 0; i < n; i++) {
+        if (a[i] == 0) {
+            continue;
         } else {
-            if (zero == 0) {
-                vals.push_back(vals.back() - 1);
-            } else {
-                vals.push_back(vals.back() + 1);
+            ll v = i + 1;
+            cur++;
+            for (ll j = i; j < n; j+=v) {
+                a[j] ^= 1;
             }
+            op[i] = 1;
         }
     }
     ll fans = 0;
     
-    // dbg(vals);
-    return vals;
-    for (ll i = 1; i < vals.size(); i++) {
-        fans+=vals[i];
+    for (auto& i : x) {
+        i--;
+        cur += ((op[i] ^ 1) == 1 ? 1 : -1);
+        op[i] ^= 1;
+        fans += cur;
+        
     }
-    // return fans;
+    return fans;
 }
-
 
 mt19937 rng((int) std::chrono::steady_clock::now().time_since_epoch().count());
 int rnd(int x, int y) {
@@ -274,41 +78,23 @@ int rnd(int x, int y) {
 auto stress1() {
     while (true) {
         ll n, q;
-        n = rnd(2, 2);
+        n = rnd(2, 10);
         vector<ll> a(n);
         for (ll i = 0; i < n; i++) {
             a[i] = rnd(0,1);
         }
-        q = rnd(1, 1);
+        q = rnd(1, 10);
         vector<ll> x(q);
         for (auto& y: x) {
             y = rnd(1, n);
         }
         auto bvals = bf(n, a, q, x);
         auto svals = solve(n, a, q, x);
-        // bool ok = true;
-        // for (ll i = 1; i < bvals.size(); i++) {
-        //     if (abs(bvals[i] - bvals[i-1]) == 1) {
-
-        //     } else {
-        //         ok = false;
-        //     }
-        // }
-        // if (ok) {
-        //     cout << "ok\n";
-        // } else {
-        //     assert(false);
-        // }
+        
         if (bvals == svals) {
             cout << "ok\n";
         } else {
             cout << "not ok\n";
-            dbg(n);
-            dbg(a);
-            dbg(q);
-            dbg(x);
-            dbg(bvals);
-            dbg(svals);
             exit(0);
         }
         
@@ -332,15 +118,8 @@ int main() {
         cin >> q;
         vector<ll> x(q);
         for (auto&y : x) cin >> y;
-        auto bans = bf(n, a, q, x);
-        auto sans = solve(n, a, q, x);
-        dbg(bans, sans);
-        for (auto& x : bans) {
-            cout << x << " ";
-        }   cout << "\n";
-        for (auto& x : sans) {
-            cout << x << " ";
-        }   cout << "\n";
-        // cout << "Case #" << tc << ": " << res << "\n";
+        auto bans = solve(n, a, q, x);
+        
+        cout << "Case #" << tc << ": " << bans << "\n";
     }
 }
